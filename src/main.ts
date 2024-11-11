d2dc19c109ba9519c65d9c56373d4ab3344316cd
nickdilo55 <ndiloren@ucsc.edu>
2024-11-11 22:29:40 +0000

    L16

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-// todo
+const button = document.createElement("button");
+button.textContent = "Click me!";
+button.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+document.body.appendChild(button);
